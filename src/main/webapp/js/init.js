102d8da4b598a98eacc7549a0fff5d441e0dcaa5
Wernnevon <wernnevon12@gmail.com>
2019-12-04 10:25:03 -0300

    front da carteira de usuário, tela de editar e visualizar perfil adicionada

--- src/main/webapp/js/init.js
+++ src/main/webapp/js/init.js
@@ -2,6 +2,7 @@
   $(function(){
     $('.dropdown-trigger').dropdown();
     $('.sidenav').sidenav();
+    $('.modal').modal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
